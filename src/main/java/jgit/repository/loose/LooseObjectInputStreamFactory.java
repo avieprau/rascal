858bdcb43ef8388fff1e3aa57407e2a4c1f05398
Andrej Viepraŭ <avieprau@gmail.com>
2009-05-21 17:10:26 +0300

    Deleted factories for creating input and output stream for git objects. Using another classes implemented with NIO instead.

--- src/main/java/jgit/repository/loose/LooseObjectInputStreamFactory.java
+++
@@ -1,7 +0,0 @@
-package jgit.repository.loose;
-
-import java.io.InputStream;
-
-public interface LooseObjectInputStreamFactory {
-    InputStream createInputStream(String objectName);
-}
--- src/main/java/jgit/repository/loose/LooseObjectOutputStreamFactory.java
+++
@@ -1,9 +0,0 @@
-package jgit.repository.loose;
-
-import org.springframework.stereotype.Component;
-
-import java.io.OutputStream;
-
-public interface LooseObjectOutputStreamFactory {
-    OutputStream createOutputStream(String objectName);
-}
