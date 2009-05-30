9265790165516a4d865201872e91f564b3d81f78
Andrej Viepraŭ <avieprau@gmail.com>
2009-05-30 17:44:37 +0300

    Integration test for loose writable byte channel.

---
+++ src/test/java/jgit/storage/loose/LooseStorageLayoutMock.java
@@ -0,0 +1,10 @@
+package jgit.storage.loose;
+
+import java.io.File;
+
+public class LooseStorageLayoutMock extends DefaultLooseStorageLayout {
+    public File getObjectsDir() {
+
+        return super.getObjectsDir();
+    }
+}
